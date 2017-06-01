8fdddf2aefedf233f1f409e160e896620e9f9d05
Kevin Liu <kkevin.liu@gmail.com>
2017-05-31 22:07:11 -0300

    simulation implemented. Run some experiments and debug (variance definition still weird)

--- main.cpp
+++ main.cpp
@@ -8,47 +8,44 @@
 #include "topology.h"
 #include "lattice.h"
 
-#define non_deterministic_seed false
+#define non_deterministic_seed true
 
 int main(int argc, char *argv[]) {
 
 	// define lattice parameters:
 	// any changes regarding topology should be done by creating a new lattice instance.
-	const int SIZE = 200;
-	const int K = 20;
+	const int SIZE = 201;
+	const int K = 100;
 	const double REWIRE_PROBABILITY = 0.0; // TODO : this is not implemented yet! only regular rings are created
 	double couplingStrength = 1.6;
 
 	// set simulation parameters
-	const size_t ITERS = 200000; // maximum amount of iterations in case system takes too long to relax
-	const size_t TRIALS = 1; // number of independent runs for each 'couplingStrength' value
+	const size_t ITERS = 50000; // maximum amount of iterations in case system takes too long to relax
+	const size_t TRIALS = 10; // number of independent runs for each 'couplingStrength' value
 
 	// prepare output filenames
 	std::ostringstream oss;
-	oss << "N=" << SIZE << "k=" << K << "p=" << REWIRE_PROBABILITY << "a=" << couplingStrength << "ITERS=" << ITERS << "TRIALS=" << TRIALS << ".txt";
+	oss << "relaxation-" << "N=" << SIZE << "k=" << K << "p=" << REWIRE_PROBABILITY << ".txt";
 	std::string relaxationFilename = oss.str();
 
-	oss << "N=" << SIZE << "k=" << K << "p=" << REWIRE_PROBABILITY << "ITERS=" << ITERS << "TRIALS=" << TRIALS << ".txt";
+	oss.str("");
+	oss << "rvsa-" << "N=" << SIZE << "k=" << K << "p=" << REWIRE_PROBABILITY << ".txt";
 	std::string rvsaFilename = oss.str();
 
-	// open files and write headers
+	// set file paths and open files
 	std::string relaxationDataPath ("relaxationData/"); // folder name for relaxation data
 	std::string rvsaDataPath ("rvsaData/"); // folder name for rvsa data
+
 	std::ofstream relaxationFile (relaxationDataPath + relaxationFilename);
 	if(!relaxationFile.is_open()) throw std::runtime_error("failed to open relaxation file. Make sure 'relaxationData' folder exists.");
 	std::ofstream rvsaFile (rvsaDataPath + rvsaFilename);
 	if(!rvsaFile.is_open()) throw std::runtime_error("failed to open rvsa file. Make sure 'rvsaData' folder exists.");
 
-	relaxationFile << "# recording of 'r' for relaxation checking. Using 'a=" << couplingStrength << "'\n";
-	relaxationFile << "# dt\tr\n";
-	rvsaFile << "# average 'r' and 'X' values after relaxation period for each value of 'a'\n";
-	rvsaFile << "# a\t<<r>>\tX=<<r2>>-<<r>>2\n";
-
 	// seed rng
 	pcg64 rng(42u, 54u);
 	if(non_deterministic_seed) rng.seed(pcg_extras::seed_seq_from<std::random_device>());
 
-	// create a lattice instance
+	// CREATE LATTICE INSTANCE
 	Lattice lattice(SIZE, K, REWIRE_PROBABILITY, couplingStrength, rng);
 
 	// print topology and initial condition FIXME: (may print incorrectly if SIZE is too big)
@@ -65,36 +62,50 @@ int main(int argc, char *argv[]) {
 		relaxationFile << dt << "\t" << r << std::endl;
 	}
 
-	// FIXME: detect relaxation and set relaxationPeriod and relaxedData to sensible values
+	// FIXME: detect relaxation and set relaxationPeriod and pointsAfterRelaxation to sensible values
 	size_t relaxationPeriod = 0;
-	size_t relaxedData = 666666666666;
+	size_t pointsAfterRelaxation = 666666666666;
 
 	// r vs a run
-	double rAvgSum = 0;
-	double r2AvgSum = 0;
-	for(size_t j = 0; j < TRIALS; ++j) {
-		double rSum = 0;
-		double r2Sum = 0;
-		double dtSum = 0;
-		for(size_t i = 0; i < ITERS && i < relaxationPeriod + relaxedData; ++i) {
-			double dt = lattice.step();
-			if(i >= relaxationPeriod) {
-				double r = lattice.getOrderParameter();
-				double r2 = r*r;
-				rSum += r*dt;
-				r2Sum += r2*dt;
-				dtSum += dt;
-			}
-		}
-		rAvgSum += rSum/dtSum;
-		r2AvgSum += r2Sum/dtSum;
+	int numPoints = 20;
+	double initialCoupling = 1.3, finalCoupling = 3.6;
+	std::vector<double> aRange (numPoints);
+	for(int i = 0; i < numPoints; ++i) {
+		aRange[i] = initialCoupling + i * (finalCoupling - initialCoupling) / numPoints;
 	}
-	double rAvgAvg = rAvgSum/TRIALS;
-	double r2AvgAvg = r2AvgSum/TRIALS;
-	double X = r2AvgAvg - rAvgAvg*rAvgAvg;
-	std::cout << "<r>2 = " << rAvgAvg*rAvgAvg << "\t<r2> = " << r2AvgAvg << std::endl;
-	rvsaFile << couplingStrength << "\t" << rAvgAvg << "\t" << X << std::endl;
 
+	// write rvsa header
+	rvsaFile << "relaxationPeriod=" << relaxationPeriod << "\tpointsAfterRelaxation=" << pointsAfterRelaxation << std::endl;
+	rvsaFile << "a\t<<r>>\tX=<<r2>> - <<r>>2\n";
+	// begin coupling strength set
+	for(const auto& a : aRange) {
+		lattice.setCouplingStrength(a);
+		double rAvgSum = 0;
+		double r2AvgSum = 0;
+		for(size_t j = 0; j < TRIALS; ++j) { // run TRIALS trials for each coupling strength
+			double rSum = 0;
+			double r2Sum = 0;
+			double dtSum = 0;
+			// each trial consists of 'ITERS' steps at most, and the first 'relaxationPeriod' steps are discarded
+			for(size_t i = 0; i < ITERS && i < relaxationPeriod + pointsAfterRelaxation; ++i) {
+				double dt = lattice.step();
+				if(i >= relaxationPeriod) {
+					double r = lattice.getOrderParameter();
+					double r2 = r*r;
+					rSum += r*dt;
+					r2Sum += r2*dt;
+					dtSum += dt;
+				}
+			}
+			rAvgSum += rSum/dtSum;
+			r2AvgSum += r2Sum/dtSum;
+		}
+		double rAvgAvg = rAvgSum/TRIALS;
+		double r2AvgAvg = r2AvgSum/TRIALS;
+		double X = r2AvgAvg - rAvgAvg*rAvgAvg;
+		std::cout << a << " finished with " << "<r>2 = " << rAvgAvg*rAvgAvg << "\t<r2> = " << r2AvgAvg << std::endl;
+		rvsaFile << a << "\t" << rAvgAvg << "\t" << X << std::endl;
+	}
 
 	return 0;
 }
