6994c67e03c2b4b70a53ca31eaa22271d9f10f6e
Kevin Liu Rodrigues <kkevin.liu@gmail.com>
2017-06-27 06:10:13 -0300

    remove vim swap files added by mistake

--- src/topology.cpp
+++ src/topology.cpp
@@ -10,15 +10,16 @@ Topology::Topology(int N, int k, double p)
 	this->k = k;
 	this->p = p;
 
-	connectome.resize((N*N-N)/2, false);
+	// connectome is a matrice where rows and columns represent
+	// lattice sites. If site i is connected to j then
+	// 	connectome[i,j] = connectome[j,i] = 1
 	kernelSizes.resize(N, 0);
 	regularRing(); // TODO generalize to rewired rings
 
 	// get minimum and maximum number of neighbors
-	int size = getNeighbors(0).size();
-	minNeighbors = maxNeighbors = size;
+	minNeighbors = maxNeighbors = kernelSizes[0];
 	for(int i = 1; i < N; ++i) {
-		size = getNeighbors(i).size();
+		int size = kernelSizes[i];
 		if(size > maxNeighbors) maxNeighbors = size;
 		else if(size < minNeighbors) minNeighbors = size;
 	}
@@ -26,17 +27,12 @@ Topology::Topology(int N, int k, double p)
 
 std::vector<int> Topology::getNeighbors(int x)
 {
-	// return all neighbors of site x by sweeping row and then column
+	// return all neighbors of site x by sweeping row
 	std::vector<int> neighbors;
 
-	for(int j = x+1; j < N; ++j) {
+	for(int j = 0; j < N; ++j) {
 		if(connectome[index(x,j)]) {
 			neighbors.push_back(j);
-		}
-	}
-	for(int i = 0; i < x; ++i) {
-		if(connectome[index(i,x)]) {
-			neighbors.push_back(i);
 		}
 	}
 
@@ -55,12 +51,13 @@ void Topology::printTopology()
 
 void Topology::regularRing()
 {
-	// allocate connectome with a regular ring topology
+	// populate connectome with regular ring connectivity
+	connectome.resize(N*N, false);
 	int idx;
 	for(int i = 0; i < N-1; ++i) {
-		for(int j = i+1; j < N; ++j) {
+		for(int j = 0; j < N; ++j) {
 			idx = index(i,j);
-			if(j <= i+k || j >= N-k+i) { // if i is connected to j increment its kernels
+			if(abs(i-j) < k && i != j) { // if i is connected to j increment its kernels
 				connectome[idx] = true;
 				++kernelSizes[i];
 				++kernelSizes[j];
@@ -71,14 +68,7 @@ void Topology::regularRing()
 }
 
 
-
 int Topology::index(int i, int j)
 {
-	// convert (i,j) notation to compact notation with bounds checking
-	if(i >= N-1 || i < 0 || j < 1 || j >= N) {
-		throw std::runtime_error("index out of bounds in topology");
-	}
-	int idx;
-	idx = i*(N-2) - (i*i-i)/2 + j - 1;
-	return idx;
+	return N*i+j;
 }
